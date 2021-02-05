e7d6033fadac0c7bcf4bb5e9203875e423a653c3
Daniel Tran <59809773+danieltran-myob@users.noreply.github.com>
2021-02-05 14:58:04 +1100

    payslip folder

---
+++ Payslip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Payslip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please input your name:  ");
+            Console.Write("Please input your surname:  ");
+            Console.Write("Please enter your annual salary:  ");
+            Console.Write("Please enter your super rate:  ");
+            Console.Write("Please enter your payment start date:  ");
+            Console.Write("Please enter your payment end date:  ");
+            Console.WriteLine("Your payslip has been generated: ");
+        }
+    }
+}
